74208ab511115e44a76e0af1cdcd5ec3925df6ae
Jonathan Soeder <=>
2009-04-03 15:21:21 -0500

    Initial Commit

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,24 @@
+class SiteController < ApplicationController
+  def index
+  end
+  
+  def about
+  
+  end
+  
+  def privacy
+  
+  end
+  
+  def terms
+  
+  end
+  
+  def contact
+  
+  end
+  
+  def faq
+    
+  end
+end
---
+++ tasks/basicpages_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :basicpages do
+#   # Task goes here
+# end
